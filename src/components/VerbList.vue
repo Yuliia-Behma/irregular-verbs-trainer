585b23015da5163a3732771a012729c45d035d3a
Juliia <juliyabegma@gmail.com>
2024-04-03 21:07:25 +0300

    Fill the VordList component

--- src/components/VerbList.vue
+++ src/components/VerbList.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="hello">
-    Hello from Verb List!
+    <table>
+      <thead>
+        <tr>
+          <th>Base form</th>
+          <th>Past simple</th>
+          <th>Past participle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list" :key="item">
+          <td>{{ item["base form"] }}</td>
+          <td>{{ item["past simple"] }}</td>
+          <td>{{ item["past participle"] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import json from '../jsonData/verbsData.json'
+
 export default {
   name: 'VerbList',
+  setup() {
+
+    const list = json.hard
   
+    return {
+      list
+    }
+  }
 }
 </script>
 
