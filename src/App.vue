85a812d81c459e2a4d66662704d1c0a4b9327f9f
Juliia <juliyabegma@gmail.com>
2024-05-03 09:16:05 +0300

    Add styles for Home component and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,188 @@
 <template>
   <header>
-    <img src="#" alt="main-icon" @click="passingToHome"/>
-    <h1>VerbFit Trainer</h1>
-    <div class="burger-menu"></div>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
+        <div class="flex items-center space-x-3 rtl:space-x-reverse">
+          <svg
+            @click="passingToHome"
+            class="w-6 h-6 text-gray-900 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M11 21V2.352A3.451 3.451 0 0 0 9.5 2a3.5 3.5 0 0 0-3.261 2.238A3.5 3.5 0 0 0 4.04 8.015a3.518 3.518 0 0 0-.766 1.128c-.042.1-.064.209-.1.313a3.34 3.34 0 0 0-.106.344 3.463 3.463 0 0 0 .02 1.468A4.017 4.017 0 0 0 2.3 12.5l-.015.036a3.861 3.861 0 0 0-.216.779A3.968 3.968 0 0 0 2 14c.003.24.027.48.072.716a4 4 0 0 0 .235.832c.006.014.015.027.021.041a3.85 3.85 0 0 0 .417.727c.105.146.219.285.342.415.072.076.148.146.225.216.1.091.205.179.315.26.11.081.2.14.308.2.02.013.039.028.059.04v.053a3.506 3.506 0 0 0 3.03 3.469 3.426 3.426 0 0 0 4.154.577A.972.972 0 0 1 11 21Zm10.934-7.68a3.956 3.956 0 0 0-.215-.779l-.017-.038a4.016 4.016 0 0 0-.79-1.235 3.417 3.417 0 0 0 .017-1.468 3.387 3.387 0 0 0-.1-.333c-.034-.108-.057-.22-.1-.324a3.517 3.517 0 0 0-.766-1.128 3.5 3.5 0 0 0-2.202-3.777A3.5 3.5 0 0 0 14.5 2a3.451 3.451 0 0 0-1.5.352V21a.972.972 0 0 1-.184.546 3.426 3.426 0 0 0 4.154-.577A3.506 3.506 0 0 0 20 17.5v-.049c.02-.012.039-.027.059-.04.106-.064.208-.13.308-.2s.214-.169.315-.26c.077-.07.153-.14.225-.216a4.007 4.007 0 0 0 .459-.588c.115-.176.215-.361.3-.554.006-.014.015-.027.021-.041.087-.213.156-.434.205-.659.013-.057.024-.115.035-.173.046-.237.07-.478.073-.72a3.948 3.948 0 0 0-.066-.68Z"
+            />
+          </svg>
+
+          <span
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-gray-900"
+            >VerbFit Trainer</span
+          >
+        </div>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul
+            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          >
+            <li>
+              <RouterLink
+                to="/"
+                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+                >Home</RouterLink
+              >
+            </li>
+            <li>
+              <RouterLink
+                to="/rules"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >Rules</RouterLink
+              >
+            </li>
+            <li>
+              <RouterLink
+                to="/verbs-list"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >Catalog</RouterLink
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
   <RouterView></RouterView>
   <footer>
-    <nav>
-      <RouterLink to="/">
-        <div class="icon"></div>
-        <span class="link-name">Home</span>
-      </RouterLink>
+    <div
+      class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600"
+    >
+      <div class="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
+        <RouterLink
+          to="/"
+          class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+        >
+          <svg
+            class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
+            />
+          </svg>
+          <span
+            class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            >Home</span
+          >
+        </RouterLink>
+        <RouterLink
+          to="/rules"
+          class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+        >
+          <svg
+            class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M11 4.717c-2.286-.58-4.16-.756-7.045-.71A1.99 1.99 0 0 0 2 6v11c0 1.133.934 2.022 2.044 2.007 2.759-.038 4.5.16 6.956.791V4.717Zm2 15.081c2.456-.631 4.198-.829 6.956-.791A2.013 2.013 0 0 0 22 16.999V6a1.99 1.99 0 0 0-1.955-1.993c-2.885-.046-4.76.13-7.045.71v15.081Z"
+            />
+          </svg>
 
-      <RouterLink to="/rules">
-        <div class="icon"></div>
-        <span class="link-name">Rules</span>
-      </RouterLink>
+          <span
+            class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            >Rules</span
+          >
+        </RouterLink>
+        <RouterLink
+          to="/verbs-list"
+          class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+        >
+          <svg
+            class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M5.005 10.19a1 1 0 0 1 1 1v.233l5.998 3.464L18 11.423v-.232a1 1 0 1 1 2 0V12a1 1 0 0 1-.5.866l-6.997 4.042a1 1 0 0 1-1 0l-6.998-4.042a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1ZM5 15.15a1 1 0 0 1 1 1v.232l5.997 3.464 5.998-3.464v-.232a1 1 0 1 1 2 0v.81a1 1 0 0 1-.5.865l-6.998 4.042a1 1 0 0 1-1 0L4.5 17.824a1 1 0 0 1-.5-.866v-.81a1 1 0 0 1 1-1Z"
+              clip-rule="evenodd"
+            />
+            <path
+              d="M12.503 2.134a1 1 0 0 0-1 0L4.501 6.17A1 1 0 0 0 4.5 7.902l7.002 4.047a1 1 0 0 0 1 0l6.998-4.04a1 1 0 0 0 0-1.732l-6.997-4.042Z"
+            />
+          </svg>
 
-      <RouterLink to="/verbs-list"
-        ><div class="icon"></div>
-        <span class="link-name">Catalog</span>
-      </RouterLink
-      >
-    </nav>
+          <span
+            class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            >Catalog</span
+          >
+        </RouterLink>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
-import router from '@/router';
+import router from "@/router";
 
 export default {
   name: "App",
   setup() {
     function passingToHome() {
-      router.replace('/');
+      router.replace("/");
     }
     return {
-      passingToHome
-    }
-  }
+      passingToHome,
+    };
+  },
 };
 </script>
 
 <style>
-.router-link-active{
-  color: rgb(64, 153, 235);
+.router-link-active {
+  /* color: rgb(64, 153, 235); */
+  color: #1c64f2;
 }
-header{
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-footer nav{
+/* footer nav {
   width: 100%;
   display: flex;
   justify-content: space-around;
   position: fixed;
   bottom: 0;
-}
-/* body{
-  margin: 0;
 } */
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,47 @@
 <script setup>
-import router from '@/router';
+import router from "@/router";
 
 function passing() {
-  router.replace('/choose-level');
+  router.replace("/choose-level");
 }
 </script>
 
 <template>
-  <div class="hello">
-    <h2>Greetings, friend!</h2>
-    <p>Dive into the wonderful world of English with our app!</p>
-    <p>Learn irregular verbs with ease and fun. Let's make your learning enjoyable and productive together!</p>
-    <button @click="passing">
-      <span>Start</span>
-      <!-- <img src="#" alt="icon"> -->
+  <div class="px-6 py-6">
+    <h3 class="text-3xl text-gray-900 font-bold dark:text-white">
+      Greetings, friend!
+    </h3>
+    <p class="text-gray-800 dark:text-gray-400">
+      Dive into the wonderful world of English with our app!
+    </p>
+    <p class="text-gray-800 dark:text-gray-400">
+      Learn irregular verbs with ease and fun. Let's make your learning
+      enjoyable and productive together!
+    </p>
+
+    <button
+      type="button"
+      @click="passing"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center max-w-sm w-full dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Start 
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M19.0148 10.2908L7.01057 3.27275C6.70544 3.09547 6.35914 3.00142 6.00629 3.00002C5.65344 2.99861 5.3064 3.0899 4.99987 3.26475C4.69542 3.43574 4.44218 3.68504 4.26639 3.98684C4.09059 4.28863 3.99863 4.63195 4.00002 4.98125V19.0174C3.99863 19.3667 4.09059 19.71 4.26639 20.0118C4.44218 20.3136 4.69542 20.5629 4.99987 20.7339C5.30629 20.9096 5.6536 21.0014 6.00679 21C6.35998 20.9986 6.70656 20.904 7.01157 20.7259L19.0168 13.7078C19.3159 13.5341 19.5641 13.2849 19.7366 12.985C19.9092 12.6852 20 12.3453 20 11.9993C20 11.6533 19.9092 11.3134 19.7366 11.0136C19.5641 10.7138 19.3159 10.4645 19.0168 10.2908H19.0148ZM5.99972 19.0174V4.98125L18.0049 11.9833L5.99972 19.0174Z"
+          fill="white"
+        />
+      </svg>
     </button>
-    
+    <!-- <button @click="passing">
+      <span>Start</span>
+    </button> -->
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
