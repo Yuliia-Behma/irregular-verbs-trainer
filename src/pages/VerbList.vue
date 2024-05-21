df8f307c806fc5239bc2cd9d9ecbded7a46eeb0f
Juliia <juliyabegma@gmail.com>
2024-05-21 15:54:17 +0300

    fix scrollbar

--- src/pages/VerbList.vue
+++ src/pages/VerbList.vue
@@ -1,11 +1,38 @@
 <template>
-  <div class="hello">
-    <div class="title">
-      <h2>Verb catalog</h2>
-      <button @click="sortList">sort (a-z)</button>
+  <div
+    class="wraper px-6 pb-4 pt-6 flex flex-col justify-start w-full max-w-md my-0 mx-auto relative"
+  >
+    <div class="flex flex-row justify-between items-baseline">
+      <h3 class="text-2xl text-gray-900 font-bold dark:text-white pb-0 pt-2">
+        Verb catalog
+      </h3>
+      <button
+        type="button"
+        @click="sortList"
+        class="text-gray-700 border border-gray-700 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-3 py-2 text-center inline-flex justify-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+      >
+        Sort (A-Z)
+        <svg
+          class="w-[16px] h-[16px] text-gray-700 dark:text-white ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8 20V7m0 13-4-4m4 4 4-4m4-12v13m0-13 4 4m-4-4-4 4"
+          />
+        </svg>
+      </button>
     </div>
 
-    <table class="main-table">
+    <!-- <table class="main-table">
       <tr>
         <td>
           <table class="header-table">
@@ -30,7 +57,29 @@
           </div>
         </td>
       </tr>
-    </table>
+    </table> -->
+    <div class="tableWraper mt-10 rounded-lg overflow-y-auto">
+      <table
+        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+      >
+        <thead
+          class="text-xs text-gray-500 uppercase text-center bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0"
+        >
+          <tr class="border-b">
+            <th scope="col" class="p-4 bg-yellow-50">Base form</th>
+            <th scope="col" class="p-4 bg-green-50">Past simple</th>
+            <th scope="col" class="p-4 bg-blue-50">Past participle</th>
+          </tr>
+        </thead>
+        <tbody class="text-sm text-gray-900 overflow-y-auto">
+          <tr v-for="item in list" :key="item">
+            <td class="p-4">{{ item["base form"] }}</td>
+            <td class="p-4 font-medium">{{ item["past simple"] }}</td>
+            <td class="p-4 font-semibold">{{ item["past participle"] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -64,42 +113,15 @@ export default {
 </script>
 
 <style scoped>
-.title {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.tableWraper {
+  height: calc(100% - 86px);
+  scrollbar-width: none;
+}
+::-webkit-scrollbar {
+  width: 0;
 }
 
-.main-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-.header-table {
-  width: 100%;
-  border-bottom: 1px solid black;
-}
-
-.isolated-div {
-  width: 100%;
-  height: 75vh;
-  overflow-y: scroll;
-}
-
-.body-table > :nth-child(odd) {
-  background-color: lightgreen;
-}
-
-.body-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-th,
-td {
-  width: 33.33%;
-}
-th {
-  text-align: start;
+tbody > :nth-child(even) {
+  background-color:  #f3f4f6;
 }
 </style>
