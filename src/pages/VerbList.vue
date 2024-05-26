a1334da09332e2a12b4e573e367b1149244f6046
Juliia <juliyabegma@gmail.com>
2024-05-26 14:39:06 +0300

    Fix appearence for Sort btn

--- src/pages/VerbList.vue
+++ src/pages/VerbList.vue
@@ -9,7 +9,7 @@
       <button
         type="button"
         @click="sortList"
-        class="text-gray-700 border border-gray-700 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-3 py-2 text-center inline-flex justify-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+        class="text-gray-700 border border-gray-700 hover:bg-gray-200 focus:outline-none font-medium rounded-lg text-xs px-3 py-2 text-center inline-flex justify-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
       >
         Sort (A-Z)
         <svg
