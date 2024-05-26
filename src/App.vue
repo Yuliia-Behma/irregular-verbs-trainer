1e77de05c0bcd440fa9a313429470690b36f4014
Juliia <juliyabegma@gmail.com>
2024-05-26 19:21:00 +0300

    Final fixing the burger menu

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,9 @@
           >
         </div>
         <button
-          id="triggerEl"
           @click="toggleMenu"
-          data-collapse-toggle="navbar-default"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          
         >
           <span class="sr-only">Open main menu</span>
           <svg
@@ -58,7 +55,6 @@
         <div
           :class="{ hidden: isMenuOpen === false, block: isMenuOpen === true }"
           class="w-full md:block md:w-auto"
-          id="navbar-default"
         >
           <ul
             class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
@@ -68,7 +64,6 @@
                 @click="closeMenu"
                 to="/"
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                aria-current="page"
                 >Home</RouterLink
               >
             </li>
