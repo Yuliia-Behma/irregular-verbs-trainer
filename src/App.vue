ec18f68565b73fa5a41f5e45f0952c99d9cd95ef
Juliia <juliyabegma@gmail.com>
2024-05-26 19:11:51 +0300

    Fix burger menu

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,7 @@
           data-collapse-toggle="navbar-default"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-900 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
+          
         >
           <span class="sr-only">Open main menu</span>
           <svg
