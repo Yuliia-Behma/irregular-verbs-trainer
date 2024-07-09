71f326eee140e6e9ffffbec8add2c7215ca1b423
Juliia <juliyabegma@gmail.com>
2024-07-09 18:38:52 +0300

    change css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/main.css';
 
 import { createPinia } from 'pinia'
 const pinia = createPinia()
